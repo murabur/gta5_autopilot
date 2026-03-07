a7b8c308cb415823865436c3f726bcd7751a6661
Murat Burc <muratburcgenel2@gmail.com>
2026-03-07 10:17:28 +0300

    style: cv2.waitKey içni yapılan yorum satırlarının stili düzenlendi

--- Bolum_1_Temeller/Video_1.3_OpenCV/2_3_kameradan_goruntu_okuma.py
+++ Bolum_1_Temeller/Video_1.3_OpenCV/2_3_kameradan_goruntu_okuma.py
@@ -11,7 +11,8 @@ while True:
     cv2.imshow("Kamera", frame)         #"Kamera" ile pencere ismi belirlenir. sağdaki kısma frame değişkeni yazılarak görüntü matrisi bastırılır.
 
     
-    #görüntüyü 1ms ekranda bekletir. 0 olursa tek bir görüntüyü kapatılasıya dek basar. Yüksek olursa FPS'i sınırlar.  "Döngü her saniye 1000 kez dönmeye çalışır, kameranın hızı (FPS) neyse görüntü o hızda akar."
+    #görüntüyü 1ms ekranda bekletir. 0 olursa tek bir görüntüyü kapatılasıya dek basar. Yüksek olursa FPS'i sınırlar.  
+    #Döngü her saniye 1000 kez dönmeye çalışır, kameranın hızı (FPS) neyse görüntü o hızda akar.
     #& 0xFF== ord("q"): q'ya basılırsa döngüden çık. Klasik opencv yazımıdır. 
     if cv2.waitKey(1) & 0xFF == ord('q'): #Buı 
         break
