96928f807f708e197b1d76eb08e6662f21a8ff25
Murat Burc <muratburcgenel2@gmail.com>
2026-03-09 16:32:55 +0300

    deque ile fps daha yumuşak bir hale getirildi

--- YOLO/yolo_detect_v2.py
+++ YOLO/yolo_detect_v2.py
@@ -1,29 +1,38 @@
 import numpy as np
 import cv2
-from ultralytics import YOLO
 import bettercam
 import time
-
+from collections import deque
 
 capture_area = (0, 40, 1280, 760)
 camera = bettercam.create(output_color="BGR")
 
+fps_deque = deque(maxlen=60)
+
+loop_start = time.perf_counter()
 
 while True:
-    start_time = time.perf_counter()
-
-
     capture = camera.grab(region=capture_area)
     if capture is None:
         continue
 
-    end_time = time.perf_counter()
+    loop_end = time.perf_counter()
+    delta_time = loop_end - loop_start
+    loop_start = loop_end 
 
-    fps = 1 / (end_time - start_time)
-
+    if delta_time > 0:
+        fps = 1 / delta_time
+        fps_deque.append(fps)
     
-    cv2.putText(img=capture, text=f"FPS: {fps:.1f}", org=(10, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 0), thickness=2)
+    fps_smooth = np.mean(fps_deque) if fps_deque else 0
+
+    cv2.putText(img=capture, text=f"FPS: {fps_smooth:.1f}", org=(10, 50), 
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, 
+                color=(0, 255, 0), thickness=2)
+    
     cv2.imshow("capture", capture)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
+cv2.destroyAllWindows()
