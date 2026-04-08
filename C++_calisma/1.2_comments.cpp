fbdb85c23c5efccbac283c446c8acb56f1de85c9
Murat Burc <muratburcgenel2@gmail.com>
2026-04-08 09:26:58 +0300

    gitignore güncelllendi

---
+++ C++_calisma/1.2_comments.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    //Bu tek satırlı bir yorumdur
+
+    /*Bu
+    çok satırlı
+    bir yorumdur
+    */
+   /*
+   *Bu da 
+   *çok
+   *satırlı
+   *bir yorumdur.
+   *Okunulabilirlik için
+   *Başlarda * kullanılmıştır.
+   */
+
+
+    std::cout << "Bu bir test yorumudur"; //ifadeler noktalı virgülle bitmeli 
+}
